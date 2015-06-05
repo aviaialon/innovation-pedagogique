3e5ef6b5001af0ef02592b5ee88a5cab9adc3436
Avi Aialon <aviaialon@gmail.com>
2015-06-05 12:38:29 -0400

    Added menu rss news

--- mvc/partial/menu/left-menu.php
+++ mvc/partial/menu/left-menu.php
@@ -22,42 +22,9 @@
     </aside>
     <?php } ?>
     <aside class="widget widget_recent_entries" id="my_recent_posts-3">
-      <h4 class="widgettitle"><span>Recent Posts</span></h4>
-      <div class="recent-posts-widget">
-        <ul>
-          <li>
-            <div class="thumb"> <a href="#" title=""> <img src="http://www.placehold.it/85x85&amp;text=Blog" alt="image" title=""></a> </div>
-            <h4><a href="#">Holy Motors - A review of best old machines</a></h4>
-            <!-- **entry-meta-data - Starts** -->
-            <div class="entry-meta-data">
-              <p><span class="fa fa-calendar"> </span> 04 Jan 2014 </p>
-              <p><a href="#"><span class="fa fa-comment"> </span> 16 </a></p>
-            </div>
-            <!-- **entry-meta-data - Ends** --> 
-          </li>
-          <li>
-            <div class="thumb"> <a href="#" title=""> <img src="http://www.placehold.it/85x85&amp;text=Blog" alt="image" title=""></a> </div>
-            <h4><a href="#">Concept based theme with various features</a></h4>
-            <!-- **entry-meta-data - Starts** -->
-            <div class="entry-meta-data">
-              <p><span class="fa fa-calendar"> </span> 03 Aug 2014 </p>
-              <p><a href="#"><span class="fa fa-comment"> </span> 20 </a></p>
-            </div>
-            <!-- **entry-meta-data - Ends** --> 
-          </li>
-          <li>
-            <div class="thumb"> <a href="#" title=""> <img src="http://www.placehold.it/85x85&amp;text=Blog" alt="image" title=""></a> </div>
-            <h4><a href="#">Colors inspired from Nature </a></h4>
-            <!-- **entry-meta-data - Starts** -->
-            <div class="entry-meta-data">
-              <p><span class="fa fa-calendar"> </span> 15 Sep 2014 </p>
-              <p><a href="#"><span class="fa fa-comment"> </span> 24 </a></p>
-            </div>
-            <!-- **entry-meta-data - Ends** --> 
-          </li>
-        </ul>
-      </div>
+      <?php $this->renderPartial('modules::rss::feed-menu', array()); ?>
     </aside>
+    
     <aside class="widget widget_text">
       <h4 class="widgettitle">Special Features</h4>
       <div class="textwidget"> 
