8a8f398302b83c2bbadc4824160038f2d0f67e67
Avi Aialon <aviaialon@gmail.com>
2015-08-30 16:10:07 -0400

    updates on teachers page

--- mvc/views/about/enseignants.php
+++ mvc/views/about/enseignants.php
@@ -2,6 +2,14 @@
 	$Application   = \Core\Application::getInstance();
 	$assetsBaseImg = $Application->getConfigs()->get('Application.core.mvc.controller.assets.base.img');
 ?>
+<style type="text/css">
+	.dt-sc-team .image img {
+		border: solid 3px #FFF;
+		border-radius: 100% !important;
+		width: 150px !important;
+		height: 145px !important;
+	}
+</style>
 <div id="main">
   <?php $this->renderPartial('menu::breadcrumb', array()); ?>
   
@@ -21,24 +29,100 @@
                 <p>Le Projet des finissants en adaptation scolaire de l’Université de Montréal est devenu un modèle d’activité 
                     d’intégration des connaissances et compétences acquises par des finissants universitaires en éducation. 
                     Ce Projet est encadré par une équipe aguerrie. </p>
+                    
+                <p>Nous invitons les milieux scolaires et les organismes communautaires préoccupés par l’éducation des élèves handicapés ou 
+                en difficulté d’apprentissage ou d’adaptation à soumettre à nos finissantes et finissants des problèmes à résoudre. 
+                Vous verrez, ils sont formidables. À chaque année, leurs innovations orthopédagogiques sont impressionnantes.</p>    
             </div><!-- **dt-sc-team-carousel - Ends** -->
-            <!-- **dt-sc-team-carousel-wrapper - Starts** -->
-            <div class="dt-sc-team-carousel-wrapper type2">
+            <hr />
+            
+            <div class="enseignants">
+            	<div class="column dt-sc-one-fourth first">
+                    <!-- **dt-sc-team - Starts** -->
+                    <div class="dt-sc-team type3">
+                       <div class="image"><img src="<?php echo $assetsBaseImg; ?>/teachers/nm.jpg" alt="Nathalie Myara" class="bradius0"/></div>
+                        <!-- **team-details - Starts** -->
+                        <div class="team-details">
+                            <h4><a href="#">Nathalie Myara</a></h4>
+                            <p>Ph. D en psychopédagogie, professeurs associée au Département de psychopédagogie et andragogie.</p>
+                            <!-- **dt-sc-social-icons - Starts** -->
+                            <ul class="dt-sc-social-icons">
+                                <li> <a href="https://www.facebook.com/nathalie.myara.1" title="twitter"> <span class="fa fa-twitter"></span>  </a> </li>
+                                <li> <a href="#" title="facebook"> <span class="fa fa-facebook"></span>  </a> </li>
+                                <li> <a href="http://ca.linkedin.com/pub/nathalie-myara-ifergan/4a/3/18a" title="linkedin"> <span class="fa fa-linkedin"></span>  </a> </li>
+                            </ul> <!-- **dt-sc-social-icons - Ends** -->
+                        </div> <!-- **team-details - Ends** -->
+                    </div><!-- **dt-sc-team - Ends** -->
+                </div>
+                
+                <div class="column dt-sc-one-fourth ">
+                    <!-- **dt-sc-team - Starts** -->
+                    <div class="dt-sc-team type3">
+                       <div class="image"><img src="<?php echo $assetsBaseImg; ?>/teachers/jl.jpg" alt="Jacques Langevin" class="bradius0"/></div>
+                        <!-- **team-details - Starts** -->
+                        <div class="team-details">
+                            <h4><a href="#">Jacques Langevin</a></h4>
+                            <p>Ph. D en psychologie cognitive, professeur titulaire au Département de psychopédagogie et andragogie.</p>
+                            <!-- **dt-sc-social-icons - Starts** -->
+                            <ul class="dt-sc-social-icons">
+                                <li> <a href="#" title="twitter"> <span class="fa fa-twitter"></span>  </a> </li>
+                                <li> <a href="#" title="facebook"> <span class="fa fa-facebook"></span>  </a> </li>
+                                <li> <a href="#" title="linkedin"> <span class="fa fa-linkedin"></span>  </a> </li>
+                            </ul> <!-- **dt-sc-social-icons - Ends** -->
+                        </div> <!-- **team-details - Ends** -->
+                    </div><!-- **dt-sc-team - Ends** -->
+                </div>
+                
+                <div class="column dt-sc-one-fourth">
+                    <!-- **dt-sc-team - Starts** -->
+                    <div class="dt-sc-team type3">
+                       <div class="image"><img src="<?php echo $assetsBaseImg; ?>/teachers/mj.jpg" alt="Manon Jolicoeur" class="bradius0"/></div>
+                        <!-- **team-details - Starts** -->
+                        <div class="team-details">
+                            <h4><a href="#">Manon Jolicoeur</a></h4>
+                            <p>Ph.D. Éducation Chercheure au Groupe DÉFI Accessibilité</p>
+                            <!-- **dt-sc-social-icons - Starts** -->
+                            <ul class="dt-sc-social-icons">
+                                <li> <a href="#" title="twitter"> <span class="fa fa-twitter"></span>  </a> </li>
+                                <li> <a href="https://www.facebook.com/manjolicoeur" title="facebook"> <span class="fa fa-facebook"></span>  </a> </li>
+                                <li> <a href="https://ca.linkedin.com/pub/manon-jolicoeur/33/836/411" title="linkedin"> <span class="fa fa-linkedin"></span>  </a> </li>
+                            </ul> <!-- **dt-sc-social-icons - Ends** -->
+                        </div> <!-- **team-details - Ends** -->
+                    </div><!-- **dt-sc-team - Ends** -->
+                </div>
+                
+                <div class="column dt-sc-one-fourth">
+                    <!-- **dt-sc-team - Starts** -->
+                    <div class="dt-sc-team type3">
+                       <div class="image"><img src="<?php echo $assetsBaseImg; ?>/teachers/ia.png" alt="Irma Alaribe" class="bradius0"/></div>
+                        <!-- **team-details - Starts** -->
+                        <div class="team-details">
+                            <h4><a href="#">Irma Alaribe</a></h4>
+                            <p>doctorante en psychopédagogie au Groupe DÉFI Accessibilité</p>
+                            <!-- **dt-sc-social-icons - Starts** -->
+                            <ul class="dt-sc-social-icons">
+                                <li> <a href="#" title="twitter"> <span class="fa fa-twitter"></span>  </a> </li>
+                                <li> <a href="#" title="facebook"> <span class="fa fa-facebook"></span>  </a> </li>
+                                <li> <a href="#" title="linkedin"> <span class="fa fa-linkedin"></span>  </a> </li>
+                            </ul> <!-- **dt-sc-social-icons - Ends** -->
+                        </div> <!-- **team-details - Ends** -->
+                    </div><!-- **dt-sc-team - Ends** -->
+                </div>
+            </div>
+			
+                                
+            
+            
+            <?php /*?><div class="type2">
                 <div class="dt-sc-team-carousel">
-                    <div class="column dt-sc-one-column">
+                    <div class="column dt-sc-one-column first">
                         <!-- **dt-sc-team - Starts** -->
                         <div class="dt-sc-team type4">
                             <div class="image"><img src="<?php echo $assetsBaseImg; ?>/teachers/nm.jpg" alt="Nathalie Myara" class="bradius0"/></div>
                             <!-- **team-details - Starts** -->
                             <div class="team-details">
                                 <h4><a href="#">Nathalie Myara</a></h4>
-                                <p>Nathalie Myara, Ph. D.,  est  chargée de cours et chercheur régulier au 
-                                département de psychopédagogie et andragogie, à la Faculté des sciences de 
-                                l'Éducation à l'Université de Montréal. Ses recherches portent notamment 
-                                sur les services offerts aux élèves HDAA, sur les plans d’intervention et sur 
-                                l'évaluation des besoins des élèves HDAA. Ses priorités actuelles consistent la 
-                                conception et l'évaluation des plans d'intervention, et, le lien entre 
-                                l'évaluation dynamique et l'élaboration d'un plan d'intervention.</p>
+                                <p>Ph. D en psychopédagogie, professeurs associée au Département de psychopédagogie et andragogie.</p>
                                 <!-- **dt-sc-social-icons - Starts** -->
                                 <ul class="dt-sc-social-icons">
                                     <li> <a href="https://www.facebook.com/nathalie.myara.1" title="twitter"> <span class="fa fa-twitter"></span>  </a> </li>
@@ -48,6 +132,7 @@
                             </div> <!-- **team-details - Ends** -->
                         </div><!-- **dt-sc-team - Ends** -->
                     </div>
+                    <hr />
                     <div class="column dt-sc-one-column">
                         <!-- **dt-sc-team - Starts** -->
                         <div class="dt-sc-team type4">
@@ -55,15 +140,7 @@
                             <!-- **team-details - Starts** -->
                             <div class="team-details">
                                 <h4><a href="#">Jacques Langevin</a></h4>
-                                <p>Jacques Langevin, Ph.D., Professeur titulaire au département de 
-                                psychopédagogie et d'andragogie de la Faculté des sciences de l'éducation de 
-                                l'Université de Montréal. Spécialiste de la conception de produits adaptés aux 
-                                élèves qui ont des incapacités intellectuelles pour aider ces élèves à maîtriser 
-                                des habiletés essentielles à l'autonomie et ce, à l'âge approprié à chaque 
-                                habileté. Il coordonne le programme de recherche 2010-2014 du Groupe 
-                                DÉFI Accessibilité qui porte sur l'accessibilité à la lecture à l'école primaire et 
-                                l'accessibilité à l'information au regard d'élèves et de citoyens qui éprouvent 
-                                une vaste gamme de limitations cognitives.</p>
+                                <p>Ph. D en psychologie cognitive, professeur titulaire au Département de psychopédagogie et andragogie.</p>
                                 <!-- **dt-sc-social-icons - Starts** -->
                                 <ul class="dt-sc-social-icons">
                                     <li> <a href="#" title="twitter"> <span class="fa fa-twitter"></span>  </a> </li>
@@ -73,6 +150,7 @@
                             </div> <!-- **team-details - Ends** -->
                         </div><!-- **dt-sc-team - Ends** -->
                     </div>
+                    <hr />
                     <div class="column dt-sc-one-column">
                         <!-- **dt-sc-team - Starts** -->
                         <div class="dt-sc-team type4">
@@ -90,6 +168,7 @@
                             </div> <!-- **team-details - Ends** -->
                         </div><!-- **dt-sc-team - Ends** -->
                     </div>
+                    <hr />
                     <div class="column dt-sc-one-column">
                         <!-- **dt-sc-team - Starts** -->
                         <div class="dt-sc-team type4">
@@ -110,15 +189,15 @@
             
                 </div>
                         
-                <div class="carousel-arrows">
+                <!--<div class="carousel-arrows">
                     <div class="pager">
                         <a href="#"> <span> 1 </span> </a>
                         <a href="#"> <span> 2 </span> </a>
                         <a href="#"> <span> 3 </span> </a>
                     </div>
-                </div>
+                </div>-->
             
-            </div><!-- **dt-sc-team-carousel-wrapper - Ends** -->
+            </div><?php */?>
         </div>       
         
       </section>
